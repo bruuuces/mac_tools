8333349c55b98849080589ad7328ef7827a8ac82
张彬 <zhangbin@topjoy.com>
2020-02-25 20:24:10 +0800

    r修改

--- script/alias.sh
+++ script/alias.sh
@@ -2,13 +2,7 @@ alias init="sh $TOOLS_DIR/script/gen_toolsrc.sh -f && source \"$TOOLS_ENV_DIR/.t
 alias ov="cv; echo \"$UCENTER_PASSWORD\" | sudo openconnect --protocol=anyconnect --servercert $VPN_TOPJOY_SERVER_CERT --user=$UCENTER_USERNAME --passwd-on-stdin $VPN_TOPJOY_HOST_IP:$VPN_TOPJOY_PORT >/dev/null 2>&1 &"
 alias cv="ps -ax | grep -v 'sudo nohup openconnect' | grep 'openconnect' | grep -v 'grep' | awk '{print \$1}' | sudo xargs kill"
 
-r(){
-  ARG_STR=$@
-  if [ $# -eq 0 ]; then
-    ARG_STR="dev"
-  fi
-  skr cfg -l -cg -d $ARG_STR
-}
+alias r="skr cfg -l -cg -d dev"
 
 alias ud="
 cd $HOME/work/svn/doc
