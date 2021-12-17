0d26b70f2e67d4558c4321b49ebe4c89538bcf02
张彬 <zhangbin_1012@icloud.com>
2021-12-17 11:45:56 +0800

    支持多git项目

--- script/util/git_merge_request.sh
+++ script/util/git_merge_request.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+remote_url=`git remote get-url origin`
+host_url=$remote_url
+host_url_len=${#host_url}
+if [[ $remote_url == git@* ]];then
+    host_url="https://"`echo "${remote_url:4:${host_url_len}-8}" | sed 's/:/\//g'`
+elif [[ $remote_url == http://* ]];then
+    host_url=${remote_url:0:${host_url_len}-4}
+elif [[ $remote_url == https://* ]];then
+    host_url=${remote_url:0:${host_url_len}-4}
+fi
 current_branch=`git branch | sed -n '/\* /s///p'`
 
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome https://git.youle.game/pandora/backend/server/-/merge_requests/new\?merge_request\[source_branch\]\=${current_branch}\&merge_request\[target_branch\]\=$1
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome ${host_url}/-/merge_requests/new\?merge_request\[source_branch\]\=${current_branch}\&merge_request\[target_branch\]\=$1
